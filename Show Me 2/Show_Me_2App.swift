907ed871df4a8412ad1a10d547dbc557d241c4db
Caleb Elson <elsonc@oregonstate.edu>
2022-02-14 19:38:10 -0600

    Initial Commit

---
+++ Show Me 2/Show_Me_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Show_Me_2App.swift
+//  Show Me 2
+//
+//  Created by Caleb Elson on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Show_Me_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
